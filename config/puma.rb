cbdbd3900c5e4da583c32ba40cd1cad20523c9e5
artyom <a.kalmykov@tarifer.ru>
2015-10-24 23:34:06 +0300

    Puma setup3

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
 root = "/var/www/smallbutterfly/current"
 
-bind "unix:///tmp/smallbutterfly.sock"
+bind "unix:///var/www/smallbutterfly/current/tmp/smallbutterfly.sock"
 
 daemonize true
 stdout_redirect "#{root}/log/access.log", "#{root}/log/errors.log", true
 
 environment ENV['RAILS_ENV'] || 'production'
-pidfile '/tmp/smallbutterfly.pid'
+pidfile '#{root}/tmp/smallbutterfly.pid'
 
 threads 2, 16
 workers 2
