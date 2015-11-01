86d520922b4cea07ed9dcf79be195f081dbdd627
artyom <a.kalmykov@tarifer.ru>
2015-11-02 02:39:54 +0300

    Let's get started

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,17 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+$(document).ready(function() {
+  /* jQuery toggle layout */
+  $('#btnToggle').click(function(){
+    if ($(this).hasClass('on')) {
+      $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
+      $(this).removeClass('on');
+    }
+    else {
+      $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
+      $(this).addClass('on');
+    }
+  });
